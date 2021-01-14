57496d722ff919ae3eef8212122b234fb2c293a3
Aaron <fq_aaron@hotmail.com>
2021-01-14 19:50:31 +0800

    remove redundant mongodb code

--- src/main/java/io/rangermix/raptorrouter/data/DataManager.java
+++ src/main/java/io/rangermix/raptorrouter/data/DataManager.java
@@ -1,17 +1,8 @@
 package io.rangermix.raptorrouter.data;
 
-import com.mongodb.MongoClientSettings;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import io.rangermix.raptorrouter.routing.model.Agency;
 import io.rangermix.raptorrouter.routing.model.DataPackage;
 import io.rangermix.raptorrouter.util.StopWatch;
 import lombok.extern.slf4j.Slf4j;
-import org.bson.codecs.configuration.CodecRegistries;
-import org.bson.codecs.configuration.CodecRegistry;
-import org.bson.codecs.pojo.PojoCodecProvider;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
@@ -28,19 +19,6 @@ import java.io.IOException;
 
 @Slf4j
 public class DataManager {
-
-    public static void main(String[] args) {
-        var sydneyGtfsDao = getSydneyGtfsDao(getInmemoryStore());
-        DataPackage dataPackage = getDataPackage(sydneyGtfsDao);
-        MongoClient mongoClient = MongoClients.create(
-                "mongodb+srv://rangermix:NgaW5ibcTnNkCKXG@sydney.krb3w.mongodb.net/model?retryWrites=true&w=majority");
-        CodecRegistry pojoCodecRegistry = CodecRegistries.fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
-                CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build()));
-        MongoDatabase database = mongoClient.getDatabase("model").withCodecRegistry(pojoCodecRegistry);
-        MongoCollection<Agency> agencyMongoCollection = database.getCollection("agency", Agency.class);
-        agencyMongoCollection.drop();
-
-    }
 
     @NotNull
     public static DataPackage getDataPackage(GtfsMutableDao gtfsDao) {
